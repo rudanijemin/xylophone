2b916f5ce720b1a1c67735ef421d1d274e520277
jemin <rudanijemin18@gmail.com>
2021-05-19 16:07:13 +0530

    seating packeg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
 
 void main() => runApp(XylophoneApp());
 
@@ -6,9 +8,67 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note1.wav');
+                  },
+
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note2.wav');
+                },
+
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note3.wav');
+                },
+
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note4.wav');
+                },
+
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note5.wav');
+                },
+
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed:(){
+                  final player=AudioCache();
+                  player.play('note7.wav');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
